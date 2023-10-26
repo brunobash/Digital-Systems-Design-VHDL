6d45b072498a9f59639022a9adf5e88ba540e48e
bruno_net <blackhunterdev01@gmail.com>
2023-10-26 16:45:10 -0300

    Testbench

--- JK-FF/FF_JK.vhd
+++ JK-FF/FF_JK.vhd
@@ -11,5 +11,27 @@ Entity FF_JK is
 end FF_JK;
 
 Architecture FF_JK_arch of FF_JK is 
-
-end 
+begin
+    P1: process (CLK, CLR, PR)
+    variable qbuf: std_logic;
+    
+    begin 
+        if(PR = '1') then
+            qbuf := '1';
+        elsif(CLR = '1') then 
+            qbuf := '0';
+        elsif(CLK = '1' AND CLK'EVENT) then
+            if(J = '0' and K = '0') then
+                qbuf := qbuf;
+            elsif(J = '1' AND K = '1') then
+                qbuf := NOT qbuf;
+            elsif(J = '0' AND K = '1') then
+                qbuf := '0';
+            else
+                qbuf := '1';
+            end if;
+        end if;
+        
+        S <= qbuf;
+    end process;
+end FF_JK_arch;
