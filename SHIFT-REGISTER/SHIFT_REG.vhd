e35739856867912146a1e43640694d7d9088ef6c
bruno_net <blackhunterdev01@gmail.com>
2023-10-27 16:42:20 -0300

    Finished shift-regiter main file

--- SHIFT-REGISTER/SHIFT_REG.vhd
+++ SHIFT-REGISTER/SHIFT_REG.vhd
@@ -15,6 +15,18 @@ Architecture SHIFT_REG_arch of SHIFT_REG is
     begin
         process(CLK, RST)
         begin
-            if RISING_EDGE(CLK)
-
+        if RISING_EDGE(CLK) then
+            if RST = '1' then 
+                qbuf <= "0000";
+            elsif LOAD = '1' then 
+                    qbuf <= D;
+            elsif DIR = '0' then 
+                qbuf <= qbuf(2 downto 0) & 1;
+            elsif DIR = '1' then 
+                qbuf <= R & qbuf(3 downto 0);
+            end if;
+            else qbuf <= qbuf;
+        end if;
+    end process;
+    Q <= qbuf;
 end SHIFT_REG_arch;
