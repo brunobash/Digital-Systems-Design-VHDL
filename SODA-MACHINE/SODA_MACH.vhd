20af46a2b4639e5b0fc750c9818499cfcaf98bac
bruno_net <blackhunterdev01@gmail.com>
2023-10-31 18:49:20 -0300

    some modifications and a new dir

---
+++ SODA-MACHINE/SODA_MACH.vhd
@@ -0,0 +1,11 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164;
+
+Entity SodaMachine is 
+	port(
+		A: in STD_LOGIC_VECTOR(1 downto 0);
+		CLK, RESET: in STD_LOGIC;
+		R: out STD_LOGIC;
+		ch25: out STD_LOGIC;
+		ch50: out STD_LOGIC;
+	    );
