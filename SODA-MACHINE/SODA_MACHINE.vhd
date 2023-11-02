ae0f41ae31851f8f474b2bcffd80bcde39b4f8e9
bruno_net <blackhunterdev01@gmail.com>
2023-11-02 19:25:20 -0300

    pushing some modifications...

--- SODA-MACHINE/SODA_MACHINE.vhd
+++ SODA-MACHINE/SODA_MACHINE.vhd
@@ -28,129 +28,128 @@ begin
 	begin
 		case currentState is 
 			when start =>
-			R <= '0';
-			tr25 <= '0';
-			tr50 <= '0';
-			if(A = "01") then
-				nextState <= twenty5;
-			elsif(A = "10") then
-				nextState <= fifty;
-			elsif(A - "11") then 
-				nextState <= start;
-			else 
-				nextState <= start;
-			end if;
+				R <= '0';
+				tr25 <= '0';
+				tr50 <= '0';
+				if(A = "01") then
+					nextState <= twenty5;
+				elsif(A = "10") then
+					nextState <= fifty;
+				elsif(A - "11") then 
+					nextState <= start;
+				else 
+					nextState <= start;
+				end if;
 
-		when twenty5 =>
-			R <= '0';
-			tr25 <= '0';
-			tr50 <= '0';
-			if(A = "01") then
-				nextState <= fifty;
-			elsif(A = "10") then
-				nextState <= seventy5;
-			elsif(A = "11") then
-				nextState <= minus25;
-			else
-				nextState <= twenty5;
-			end if
-		when fifty =>
-			R <= '0';
-			tr25 <= '0';
-			tr50 <= '0';
-			if(A = "01") then
-				nextState <= seventy5;
-			elsif(A = "10") then
-				nextState <= one;
-			elsif(A = "11") then
-				nextState <= minus50;
-			else
-				nextState <= fifty;
-			end if;
-		when seventy5 =>
-			R <= '0';
-			tr25 <= '0';
-			tr50 <= '0';
-			if(A = "01") then
-				nextState <= one;
-			elsif(A = "10") then
-				nextState <= one25;
-			elsif(A = "11") then
-				nextState <= minus75;
-			else
-				nextState <= seventy5;
-			end if;
-		when one => 
-			R <= '1';
-			tr25 <= '0';
-			tr50 <= '0';
-			if(A = "01") then
-				nextState <= twenty5;
-			elsif(A = "10") then
-				nextState <= fifty;
-			elsif(A = "11") then
-				nextState <= start;
-			else 
-				nextState <= start;
-			end if;
-		when one25 =>
-			R <= '1';
-			tr25 <= '1';
-			tr50 <= '0';
-			if(A = "01") then
-				nextState <= twenty5;
-			elsif(A = "10") then 
-				nextState <= fifty;
-			elsif(A = "11") then
-				nextState <= start;
-			else 
-				nextState <= one25;
-			end if;
-
-		when minus25 =>
-			R <= '0';
-			tr25 <= '1';
-			tr50 <= '0';
-			if(A = "01") then
-				nextState <= twenty5;
-			elsif(A = "10") then 
-				nextState <= fifty;
-			elsif(A = "11") then
-				nextState <= start;
-			else
-				nextState <= start;
-			end if;
-		when minus50 =>
-			R <= '0';
-			tr25 <= '0';
-			tr50 <= '1';
-			if(A = "01") then
-				nextState <= twenty5;
-			elsif(A = "10") then
-				nextState <= fifty;
-			elsif(A = "11") then
-				nextState <= start;
-			else 
-				nextState <= start;
-			end if;
-		when minus75 => 
-			R <= '0';
-			tr25 <= '1';
-			tr50 <= '1';
-			if(A = "01") then
-				nextState <= twenty5;
-			elsif(A = "10") then
-				nextState <= fifty;
-			elsif(A = "11") then
-				nextState <= start;
-			else
-				nextState <= start;
-			end if;
-		when others =>
-			R <= '0';
-			tr25 <= '0';
-			tr50 <= '0';
-	end case;
-end process comb_proc;
+			when twenty5 =>
+				R <= '0';
+				tr25 <= '0';
+				tr50 <= '0';
+				if(A = "01") then
+					nextState <= fifty;
+				elsif(A = "10") then
+					nextState <= seventy5;
+				elsif(A = "11") then
+					nextState <= minus25;
+				else
+					nextState <= twenty5;
+				end if
+			when fifty =>
+				R <= '0';
+				tr25 <= '0';
+				tr50 <= '0';
+				if(A = "01") then
+					nextState <= seventy5;
+				elsif(A = "10") then
+					nextState <= one;
+				elsif(A = "11") then
+					nextState <= minus50;
+				else
+					nextState <= fifty;
+				end if;
+			when seventy5 =>
+				R <= '0';
+				tr25 <= '0';
+				tr50 <= '0';
+				if(A = "01") then
+					nextState <= one;
+				elsif(A = "10") then
+					nextState <= one25;
+				elsif(A = "11") then
+					nextState <= minus75;
+				else
+					nextState <= seventy5;
+				end if;
+			when one => 
+				R <= '1';
+				tr25 <= '0';
+				tr50 <= '0';
+				if(A = "01") then
+					nextState <= twenty5;
+				elsif(A = "10") then
+					nextState <= fifty;
+				elsif(A = "11") then
+					nextState <= start;
+				else 
+					nextState <= start;
+				end if;
+			when one25 =>
+				R <= '1';
+				tr25 <= '1';
+				tr50 <= '0';
+				if(A = "01") then
+					nextState <= twenty5;
+				elsif(A = "10") then 
+					nextState <= fifty;
+				elsif(A = "11") then
+					nextState <= start;
+				else 
+					nextState <= one25;
+				end if;
+			when minus25 =>
+				R <= '0';
+				tr25 <= '1';
+				tr50 <= '0';
+				if(A = "01") then
+					nextState <= twenty5;
+				elsif(A = "10") then 
+					nextState <= fifty;
+				elsif(A = "11") then
+					nextState <= start;
+				else
+					nextState <= start;
+				end if;
+			when minus50 =>
+				R <= '0';
+				tr25 <= '0';
+				tr50 <= '1';
+				if(A = "01") then
+					nextState <= twenty5;
+				elsif(A = "10") then
+					nextState <= fifty;
+				elsif(A = "11") then
+					nextState <= start;
+				else 
+					nextState <= start;
+				end if;
+			when minus75 => 
+				R <= '0';
+				tr25 <= '1';
+				tr50 <= '1';
+				if(A = "01") then
+					nextState <= twenty5;
+				elsif(A = "10") then
+					nextState <= fifty;
+				elsif(A = "11") then
+					nextState <= start;
+				else
+					nextState <= start;
+				end if;
+			when others =>
+				R <= '0';
+				tr25 <= '0';
+				tr50 <= '0';
+		end case;
+	end process comb_process;
 end SodaMachine_arch;
 
---
+++ SODA-MACHINE/SODA_MACHINE_tb.vhd
@@ -0,0 +1,116 @@
+library IEEE:
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_SIGNED.ALL;
+use STD.TEXTIO.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+
+Entity SodaMachine_tb is 
+end SodaMachine_tb;
+
+Architecture SodaMachine_tb_arch of SodaMachine_tb is
+    component SodaMachine port(
+        A: in STD_LOGIC_VECTOR(1 downto 0);
+        CLK, RESET: in STD_LOGIC;
+        R: out STD_LOGIC
+    );
+    end component;
+
+    constant G: time := 2 ns;
+
+    signal A: std_logic_vector(1 downto 0);
+    signal CLK, RESET: std_logic;
+    signal R, S, T: std_logic;
+
+    begin
+        soda: SodaMachine port map(
+            A => A, 
+            CLK => CLK,
+            R => R
+        );
+
+        clock: process
+        begin
+            CLK <= '0', '1' after G/2, '0' after G;
+            wait for G;
+
+        end process;
+
+        proc: process
+        begin 
+        A <= "00";
+        for i in 1 to 10 loop
+            wait for 6 ns;
+            A <= unsigned(A) + 1;
+        end loop;
+
+        wait for 1 ns;
+
+        RESET <= '1';
+        wait for 3 ns;
+
+        RESET <= '0';
+
+        A <= "01";
+        for i in 1 to 10 loop
+            wait for 6 ns;
+            A <= unsigned(A) + 1;
+        end loop;
+
+        wait for 1 ns;
+
+        RESET <= '1';
+        wait for 3 ns;
+
+        RESET <= '0';
+
+        A <= "10";
+        for i in 1 to 10 loop
+            wait for 7 ns;
+            A <= unsigned(A) + 1;
+        end loop;
+
+        wait for 1 ns;
+        RESET <= '1';
+        wait for 3 ns;
+        RESET <= '0';
+
+        A <= "11";
+        for i in 1 to 10 loop
+            wait for 6 ns;
+            A <= unsigned(A) + 1;
+        end loop;
+
+        wait for 1 ns;
+        RESET <= '1';
+        wait for 3 ns;
+        RESET <= '0';
+
+        A <= "11";
+        for i in 1 to 3 loop
+            wait for 6 ns;
+            A <= unsigned(A) + 10;
+        end loop;
+
+        A <= "00";
+
+        for i in 1 to 3 loop
+            wait for 6 ns;
+            A <= unsigned(A) + 1;
+        end loop;
+
+        wait fo 1 ns;
+
+        A <= "11";
+        wait for 6 ns;
+        A <= "00";
+
+        wait for 1 ns;
+        RESET <= '1';
+        wait for 3 ns;
+        RESET <= '0';
+        wait;
+    end process;
+end SodaMachine_tb_arch; 
+
+
+
